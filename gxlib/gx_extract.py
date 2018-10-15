450f96f78699b06ba26dbe71397dfc0064247fb3
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2018-10-15 23:59:35 +1100

    corrected indices in filter

--- gxlib/gx_extract.py
+++ gxlib/gx_extract.py
@@ -142,11 +142,11 @@ def _filter_tdps(tdps,std_coeff=3):
         #Step 1. Filtering sigmas - fileter X Y Z sigmas based on <= 3*std, and <1
         filt1_data = tdp [
             #Sigma X
-            (tdp['sigma'][0]<=(_np.median(tdp['sigma'][0])+ std_coeff*_np.std(tdp['sigma'][0])))&
+            (tdp['sigma'].iloc[0]<=(_np.median(tdp['sigma'].iloc[0])+ std_coeff*_np.std(tdp['sigma'].iloc[0])))&
             #Sigma Y
-            (tdp['sigma'][1]<=(_np.median(tdp['sigma'][1])+ std_coeff*_np.std(tdp['sigma'][1])))&
+            (tdp['sigma'].iloc[1]<=(_np.median(tdp['sigma'].iloc[1])+ std_coeff*_np.std(tdp['sigma'].iloc[1])))&
             #Sigma Z
-            (tdp['sigma'][2]<=(_np.median(tdp['sigma'][2])+ std_coeff*_np.std(tdp['sigma'][2])))
+            (tdp['sigma'].iloc[2]<=(_np.median(tdp['sigma'].iloc[2])+ std_coeff*_np.std(tdp['sigma'].iloc[2])))
             # &
             # #Sigma X
             # (tdp.iloc[:,10]<=1)&
@@ -158,28 +158,26 @@ def _filter_tdps(tdps,std_coeff=3):
         #Step 2. Filtering values - fileter X Y Z values (m) based on -3*std<Value<+3*std
         filt2_data = filt1_data [
             #Value X
-            ((filt1_data['value'][0]<=(_np.median(filt1_data['value'][0])+ std_coeff*_np.std(filt1_data['value'][0])))
-                                    &(filt1_data['value'][0]>=(_np.median(filt1_data['value'][0])- std_coeff*_np.std(filt1_data['value'][0]))))
+            ((filt1_data['value'].iloc[0]<=(_np.median(filt1_data['value'].iloc[0])+ std_coeff*_np.std(filt1_data['value'].iloc[0])))
+                                    &(filt1_data['value'].iloc[0]>=(_np.median(filt1_data['value'].iloc[0])- std_coeff*_np.std(filt1_data['value'].iloc[0]))))
             &
             #Value Y
-            ((filt1_data['value'][1]<=(_np.median(filt1_data['value'][1])+ std_coeff*_np.std(filt1_data['value'][1])))
-                                    &(filt1_data['value'][1]>=(_np.median(filt1_data['value'][1])- std_coeff*_np.std(filt1_data['value'][1]))))
+            ((filt1_data['value'].iloc[1]<=(_np.median(filt1_data['value'].iloc[1])+ std_coeff*_np.std(filt1_data['value'].iloc[1])))
+                                    &(filt1_data['value'].iloc[1]>=(_np.median(filt1_data['value'].iloc[1])- std_coeff*_np.std(filt1_data['value'].iloc[1]))))
             &
             #Value Z
-            ((filt1_data['value'][2]<=(_np.median(filt1_data['value'][2])+ std_coeff*_np.std(filt1_data['value'][2])))
-                                    &(filt1_data['value'][2]>=(_np.median(filt1_data['value'][2])- std_coeff*_np.std(filt1_data['value'][2]))))
+            ((filt1_data['value'].iloc[2]<=(_np.median(filt1_data['value'].iloc[2])+ std_coeff*_np.std(filt1_data['value'].iloc[2])))
+                                    &(filt1_data['value'].iloc[2]>=(_np.median(filt1_data['value'].iloc[2])- std_coeff*_np.std(filt1_data['value'].iloc[2]))))
             ]
         #Step 3. Second pass over sigma values
         filt3_data = filt2_data[
             #Sigma X
-            (filt2_data['sigma'][0]<=(_np.median(filt2_data['sigma'][0])+ std_coeff*_np.std(filt2_data['sigma'][0])))&
+            (filt2_data['sigma'].iloc[0]<=(_np.median(filt2_data['sigma'].iloc[0])+ std_coeff*_np.std(filt2_data['sigma'].iloc[0])))&
             #Sigma Y
-            (filt2_data['sigma'][1]<=(_np.median(filt2_data['sigma'][1])+ std_coeff*_np.std(filt2_data['sigma'][1])))&
+            (filt2_data['sigma'].iloc[1]<=(_np.median(filt2_data['sigma'].iloc[1])+ std_coeff*_np.std(filt2_data['sigma'].iloc[1])))&
             #Sigma Z
-            (filt2_data['sigma'][2]<=(_np.median(filt2_data['sigma'][2])+ std_coeff*_np.std(filt2_data['sigma'][2])))
+            (filt2_data['sigma'].iloc[2]<=(_np.median(filt2_data['sigma'].iloc[2])+ std_coeff*_np.std(filt2_data['sigma'].iloc[2])))
             ]
-
-
 
         filtered_dataset[i] = filt3_data
     return filtered_dataset
