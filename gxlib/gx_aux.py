bac6a9c2821dd0edbe3171a47a5bd7d6627c5d18
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2018-10-15 16:11:51 +1100

    gen_staDb checks if file already exists

--- gxlib/gx_aux.py
+++ gxlib/gx_aux.py
@@ -16,49 +16,55 @@ def gen_staDb(tmp_dir,project_name,stations_list,IGS_logs_dir):
     #Making staDb directory in tmp folder 
     staDb_dir = tmp_dir + '/staDb/' + project_name + '/'
     staDb_file = staDb_dir + project_name + '.staDb'
-    if not _os.path.exists(staDb_dir):
-        _os.makedirs(staDb_dir)
-    #getting paths to all log files needed    
-    logs = _np.ndarray((len(stations_list)),dtype=object)
-    for i in range(len(stations_list)):
-        logs[i] = _glob.glob(IGS_logs_dir + '/*/' + stations_list[i].lower() +'*')[0]
 
-    with open(staDb_file,'w') as output:
-        output.write("KEYWORDS: ID STATE ANT RX\n")  # POSTSEISMIC, LINK, END
-        for file in logs:
-            with open(file, 'r') as f:
-                data = f.read()
-        # Site ID
-            matches_ID = _re.findall(_regex_ID, data)
-        # Site Location, only one location line per BIGF log
-            matches_loc = _re.findall(_regex_loc, data)
-            output.write("{ID}  ID  {IERS} {loc_2} {loc_1}\n".format(ID=matches_ID[0][1], IERS=matches_ID[0][3] if matches_ID[0][3] != '' else 'NONE',
-                                                            loc_2=matches_loc[0][1], loc_1=matches_loc[0][2]))
+    #if staDb_file was already generated, just return staDb_file path
+    if not _os.path.exists(staDb_file):
 
-            output.write("{ID}  STATE 1-01-01 00:00:00 {X:.15e}  {Y:.15e} {Z:.15e} {X_v:.15e}  {Y_v:.15e} {Z_v:.15e}\n".format(ID=matches_ID[0][1],
-                                                                                                                      X=float(matches_loc[0][4]) if matches_loc[0][4] != '' else 0,
-                                                                                                                      Y=float(matches_loc[0][5]) if matches_loc[0][5] != '' else 0,
-                                                                                                                      Z=float(matches_loc[0][6]) if matches_loc[0][6] != '' else 0,
-                                                                                                                      X_v=0, Y_v=0, Z_v=0))
-        # Receiver Information
-            rec = []
-            matches_rec = _re.finditer(_regex_rec, data)
-            for matchNum, match in enumerate(matches_rec):
-                for groupNum in range(0, len(match.groups())):
-                    groupNum = groupNum + 1
-                rec.append(match.groups())
-                output.write("{ID}  RX {d_inst} {t_inst}:00 {rec_type} {rec_num} {rec_fw_v}\n".format(ID=matches_ID[0][1], d_inst=rec[matchNum][5], t_inst=rec[
-                      matchNum][7] if rec[matchNum][7] != '' else '00:00', rec_type=rec[matchNum][0], rec_num=rec[matchNum][2], rec_fw_v=rec[matchNum][3]))
-        # Antenna Information
-            ant = []
-            matches_ant = _re.finditer(_regex_ant, data)
-            for matchNum, match in enumerate(matches_ant):
-                for groupNum in range(0, len(match.groups())):
-                    groupNum = groupNum + 1
-                ant.append(match.groups())
-                output.write("{ID}  ANT {d_inst} {t_inst}:00 {ant_type} {radome_type} {up} {north} {east} {ant_num}\n".
-                      format(ID=matches_ID[0][1], d_inst=ant[matchNum][12], t_inst=ant[matchNum][13] if ant[matchNum][13]!= '' else '00:00', ant_type=ant[matchNum][0],
-                             radome_type=ant[matchNum][8], up=ant[matchNum][4], north=ant[matchNum][5], east=ant[matchNum][6], ant_num=ant[matchNum][2]))
+        if not _os.path.exists(staDb_dir):
+            _os.makedirs(staDb_dir)
+        #getting paths to all log files needed    
+        logs = _np.ndarray((len(stations_list)),dtype=object)
+        for i in range(len(stations_list)):
+            logs[i] = _glob.glob(IGS_logs_dir + '/*/' + stations_list[i].lower() +'*')[0]
+
+        with open(staDb_file,'w') as output:
+            output.write("KEYWORDS: ID STATE ANT RX\n")  # POSTSEISMIC, LINK, END
+            for file in logs:
+                with open(file, 'r') as f:
+                    data = f.read()
+            # Site ID
+                matches_ID = _re.findall(_regex_ID, data)
+            # Site Location, only one location line per BIGF log
+                matches_loc = _re.findall(_regex_loc, data)
+                output.write("{ID}  ID  {IERS} {loc_2} {loc_1}\n".format(ID=matches_ID[0][1], IERS=matches_ID[0][3] if matches_ID[0][3] != '' else 'NONE',
+                                                                loc_2=matches_loc[0][1], loc_1=matches_loc[0][2]))
+
+                output.write("{ID}  STATE 1-01-01 00:00:00 {X:.15e}  {Y:.15e} {Z:.15e} {X_v:.15e}  {Y_v:.15e} {Z_v:.15e}\n".format(ID=matches_ID[0][1],
+                                                                                                                        X=float(matches_loc[0][4]) if matches_loc[0][4] != '' else 0,
+                                                                                                                        Y=float(matches_loc[0][5]) if matches_loc[0][5] != '' else 0,
+                                                                                                                        Z=float(matches_loc[0][6]) if matches_loc[0][6] != '' else 0,
+                                                                                                                        X_v=0, Y_v=0, Z_v=0))
+            # Receiver Information
+                rec = []
+                matches_rec = _re.finditer(_regex_rec, data)
+                for matchNum, match in enumerate(matches_rec):
+                    for groupNum in range(0, len(match.groups())):
+                        groupNum = groupNum + 1
+                    rec.append(match.groups())
+                    output.write("{ID}  RX {d_inst} {t_inst}:00 {rec_type} {rec_num} {rec_fw_v}\n".format(ID=matches_ID[0][1], d_inst=rec[matchNum][5], t_inst=rec[
+                        matchNum][7] if rec[matchNum][7] != '' else '00:00', rec_type=rec[matchNum][0], rec_num=rec[matchNum][2], rec_fw_v=rec[matchNum][3]))
+            # Antenna Information
+                ant = []
+                matches_ant = _re.finditer(_regex_ant, data)
+                for matchNum, match in enumerate(matches_ant):
+                    for groupNum in range(0, len(match.groups())):
+                        groupNum = groupNum + 1
+                    ant.append(match.groups())
+                    output.write("{ID}  ANT {d_inst} {t_inst}:00 {ant_type} {radome_type} {up} {north} {east} {ant_num}\n".
+                        format(ID=matches_ID[0][1], d_inst=ant[matchNum][12], t_inst=ant[matchNum][13] if ant[matchNum][13]!= '' else '00:00', ant_type=ant[matchNum][0],
+                                radome_type=ant[matchNum][8], up=ant[matchNum][4], north=ant[matchNum][5], east=ant[matchNum][6], ant_num=ant[matchNum][2]))
+    
+    
     return staDb_file
 
 def _dr_size(rnx_files_in_out):
