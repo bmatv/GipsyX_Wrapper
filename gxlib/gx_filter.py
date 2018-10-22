168ede68ca3719e5ef48e18d5d0720f2fdd31990
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2018-10-22 17:07:40 +1100

    finalised filter module

--- gxlib/gx_filter.py
+++ gxlib/gx_filter.py
@@ -1,47 +1,27 @@
-def _filter_tdps(tdps,std_coeff=3):
-    '''For now commented sigma > 1 filtering'''
-    filtered_dataset = _np.ndarray((len(tdps)),dtype=object)
-    for i in range(len(tdps)):
-        tdp = tdps[i]
-        #Step 1. Filtering sigmas - fileter X Y Z sigmas based on <= 3*std, and <1
-        filt1_data = tdp [
-            #Sigma X
-            (tdp['sigma'].iloc[:,0]<=(_np.median(tdp['sigma'].iloc[:,0])+ std_coeff*_np.std(tdp['sigma'].iloc[:,0])))&
-            #Sigma Y
-            (tdp['sigma'].iloc[:,1]<=(_np.median(tdp['sigma'].iloc[:,1])+ std_coeff*_np.std(tdp['sigma'].iloc[:,1])))&
-            #Sigma Z
-            (tdp['sigma'].iloc[:,2]<=(_np.median(tdp['sigma'].iloc[:,2])+ std_coeff*_np.std(tdp['sigma'].iloc[:,2])))
-            # &
-            # #Sigma X
-            # (tdp.iloc[:,10]<=1)&
-            # #Sigma Y
-            # (tdp.iloc[:,11]<=1)&
-            # #Sigma Z
-            # (tdp.iloc[:,12]<=1)
-            ]
-        #Step 2. Filtering values - fileter X Y Z values (m) based on -3*std<Value<+3*std
-        filt2_data = filt1_data [
-            #Value X
-            ((filt1_data['value'].iloc[:,0]<=(_np.median(filt1_data['value'].iloc[:,0])+ std_coeff*_np.std(filt1_data['value'].iloc[:,0])))
-                                    &(filt1_data['value'].iloc[:,0]>=(_np.median(filt1_data['value'].iloc[:,0])- std_coeff*_np.std(filt1_data['value'].iloc[:,0]))))
-            &
-            #Value Y
-            ((filt1_data['value'].iloc[:,1]<=(_np.median(filt1_data['value'].iloc[:,1])+ std_coeff*_np.std(filt1_data['value'].iloc[:,1])))
-                                    &(filt1_data['value'].iloc[:,1]>=(_np.median(filt1_data['value'].iloc[:,1])- std_coeff*_np.std(filt1_data['value'].iloc[:,1]))))
-            &
-            #Value Z
-            ((filt1_data['value'].iloc[:,2]<=(_np.median(filt1_data['value'].iloc[:,2])+ std_coeff*_np.std(filt1_data['value'].iloc[:,2])))
-                                    &(filt1_data['value'].iloc[:,2]>=(_np.median(filt1_data['value'].iloc[:,2])- std_coeff*_np.std(filt1_data['value'].iloc[:,2]))))
-            ]
-        #Step 3. Second pass over sigma values
-        filt3_data = filt2_data[
-            #Sigma X
-            (filt2_data['sigma'].iloc[:,0]<=(_np.median(filt2_data['sigma'].iloc[:,0])+ std_coeff*_np.std(filt2_data['sigma'].iloc[:,0])))&
-            #Sigma Y
-            (filt2_data['sigma'].iloc[:,1]<=(_np.median(filt2_data['sigma'].iloc[:,1])+ std_coeff*_np.std(filt2_data['sigma'].iloc[:,1])))&
-            #Sigma Z
-            (filt2_data['sigma'].iloc[:,2]<=(_np.median(filt2_data['sigma'].iloc[:,2])+ std_coeff*_np.std(filt2_data['sigma'].iloc[:,2])))
-            ]
+'''New filter module
+Takes the output gather of .solutions() method and does preliminary filtering
+with 0.1 derivative margin for X Y Z and 3*std margin for sigma X Y Z'''
+import numpy as _np
 
-        filtered_dataset[i] = filt3_data
-    return filtered_dataset
+def _filter_derivative(dataset, margin=0.1):
+    value_dataset = dataset['value'].iloc[:,[0,1,2]].values
+    
+    rolled_dataset =_np.roll(value_dataset,1,axis=0)
+    derivative = _np.abs(value_dataset - rolled_dataset)
+    
+    mask = (derivative<=margin).min(axis=1)
+    return dataset[mask]
+
+def _filter_sigma(dataset,std_coeff=3):
+    sigma_cut = dataset['sigma'].median() + dataset['sigma'].std()*std_coeff
+    mask = (dataset['sigma']<=sigma_cut).min(axis=1)
+    return dataset[mask]
+    
+def filter_tdps(tdps,std_coeff=3,margin=0.1):
+    filtered_tdps = _np.ndarray((tdps.shape),dtype = object)
+    for i in range(tdps.shape[0]):
+        step1_filter = _filter_derivative(dataset = tdps[i], margin = margin)
+        step2_filter = _filter_sigma(dataset = step1_filter, std_coeff=std_coeff)
+        filtered_tdps[i] = step2_filter
+        print('step1: {}% left. step2: {}% left.'.format(tdps[i].shape[0]/step1_filter.shape[0]*100, tdps[i].shape[0]/step2_filter.shape[0]*100))
+    return filtered_tdps
