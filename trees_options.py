16997c821ac2e2f71a27ea7cb0f5b3449c29a9ef
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-02-13 13:52:58 +1100

    glo_only tree added based on m4 and addition of modular elements

--- trees_options.py
+++ trees_options.py
@@ -1028,3 +1028,49 @@ penna_k_randomwalk_m4_static = [[
         ],
         #Pop values. Values that will be elemenated
         []]
+
+
+glo_only = penna_k_randomwalk_m4 + pseudo_range + carrier_phase + gps_sv_del
+#Pseudorange
+pseudo_range = [
+['Global:DataTypes:IonoFreeC_1P_2P', ''],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO', ''],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:DataBias', ''],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:DataBias:DataBiasReference', 'GLONASS'],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:DataBias:StochasticAdj', '1.0e4 3.40e-4 DATADRIVEN RANDOMWALK'],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:DataBias:StochasticAdj:UseItOrLoseItInterval', '3600'],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:PostSmoothEdit', '2e5 2e4 25 20 10 5'],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:SignalPath', ''],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GLO:SignalPath:Platforms', '.* R.*'],
+    
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GPS', ''],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GPS:ElDepWeight', 'SqrtSin'],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GPS:PostSmoothEdit', '2e5 2e4 12.5 10 5 2.5'],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GPS:SignalPath', ''],
+['Global:DataTypes:IonoFreeC_1P_2P:DataLinkSpec_PC_GPS:SignalPath:Platforms', '.* GPS.*']
+]
+                    
+#Carrier Phase                  
+carrier_phase = [
+['Global:DataTypes:IonoFreeL_1P_2P', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO:DataBias', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO:DataBias:StochasticAdj', '3.0e8 3.0e8 DATADRIVEN WHITENOISE'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO:DataBias:StochasticAdj:UseItOrLoseItInterval', '3600'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO:PostSmoothEdit', '2e5 2e4 0.25 0.2 0.1 .05'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO:SignalPath', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GLO:SignalPath:Platforms', '.* R.*'],
+    
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:DataBias', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:DataBias:StochasticAdj', '3.0e8 3.0e8 DATADRIVEN WHITENOISE'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:DataBias:StochasticAdj:UseItOrLoseItInterval', '3600'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:ElDepWeight', 'SqrtSin'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:PostSmoothEdit', '2e5 2e4 0.125 0.1 0.05 .025'],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:SignalPath', ''],
+['Global:DataTypes:IonoFreeL_1P_2P:DataLinkSpec_LC_GPS:SignalPath:Platforms', '.* GPS.*']
+]
+
+gps_sv_del = [['Satellite:Delete','GPS01 GPS02 GPS03 GPS04 GPS05 GPS06 GPS08 GPS09 GPS10 GPS11 GPS13 GPS14 GPS15 GPS16 GPS17 GPS18 GPS19 GPS20 GPS21 GPS22 GPS23 GPS24 GPS25 GPS26 GPS27 GPS28 GPS29 GPS30 GPS31 GPS32 GPS33 GPS34 GPS35 GPS36 GPS37 GPS38 GPS39 GPS40 GPS41 GPS43 GPS44 GPS45 GPS46 GPS47 GPS48 GPS49 GPS50 GPS51 GPS52 GPS53 GPS54 GPS55 GPS56 GPS57 GPS58 GPS59 GPS60 GPS61 GPS62 GPS63 GPS64 GPS65 GPS66 GPS67 GPS68 GPS69 GPS71 GPS72 GPS73 GPS70']]
+
+glo_sv_del = [['Satellite:Delete','R701 R802 R711 R712 R713 R714 R715 R716 R717 R718 R719 R720 R721 R722 R723 R724 R725 R726 R727 R728 R729 R730 R731 R732 R733 R734 R735 R736 R737 R738 R742 R743 R744 R745 R746 R747 R851 R852 R853 R856 R754 R854 R755 R855 R783 R787 R788 R789 R791 R792 R793 R794 R795 R796 R797 R798 R801']]
