83288b9fbdf1a59bad0802c9acc96e257605cbd2
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-02-19 13:24:05 +1100

    removing trop StochastcAdj as VMF1 enabled

--- trees_options.py
+++ trees_options.py
@@ -1020,7 +1020,11 @@ penna_k_randomwalk_m4 = [[
     
         ],
         #Pop values. Values that will be eliminated
-        ['GRN_STATION_CLK_WHITE:State:Pos:ConstantAdj','GRN_STATION_CLK_WHITE:Tides:OceanLoad']]
+        ['GRN_STATION_CLK_WHITE:State:Pos:ConstantAdj',
+        'GRN_STATION_CLK_WHITE:Tides:OceanLoad',
+        'GRN_STATION_CLK_WHITE:Trop:WetZ:StochasticAdj',
+        'GRN_STATION_CLK_WHITE:Trop:GradNorth:StochasticAdj',
+        'GRN_STATION_CLK_WHITE:Trop:GradEast:StochasticAdj']]
 
 #Pseudorange
 pseudo_range_glo = [['Global:DataTypes:IonoFreeC_1P_2P', ''],
