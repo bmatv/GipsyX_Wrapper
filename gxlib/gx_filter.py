dc22039aba5c168e23c5f591048eb1cf45ae65df
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-03-04 19:10:08 +1100

    _filter_clk value is 1e8 now

--- gxlib/gx_filter.py
+++ gxlib/gx_filter.py
@@ -25,7 +25,7 @@ def _filter_sigma(dataset,std_coeff=3):
     return dataset[mask]
 
 def _filter_clk(dataset):
-    mask = dataset.sigma.iloc[:,0] < 2e8
+    mask = dataset.sigma.iloc[:,0] < 1e8
     # mask =(dataset['value'].iloc[:,0].abs() > 0.01) & (dataset['value'].iloc[:,0].abs() <100)
     return dataset[mask]
     
