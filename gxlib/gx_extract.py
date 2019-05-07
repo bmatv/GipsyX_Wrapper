0c6c3bc44256365635a53c050f98d8fd234f16b2
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-05-07 15:13:36 +1000

    tqdm switch for gather_solutions

--- gxlib/gx_extract.py
+++ gxlib/gx_extract.py
@@ -8,7 +8,7 @@ import tqdm as _tqdm
 import blosc as _blosc
 
 '''Extraction of solutions from npz'''
-def gather_solutions(tmp_dir,project_name,stations_list,num_cores):
+def gather_solutions(tmp_dir,project_name,stations_list,num_cores,tqdm):
     checked_stations = _check_stations(stations_list = stations_list,tmp_dir=tmp_dir,project_name=project_name)
     n_stations = len(checked_stations)
     #Create a list of paths to get data from
@@ -21,7 +21,7 @@ def gather_solutions(tmp_dir,project_name,stations_list,num_cores):
     for i in range(n_stations):
         if not _os.path.exists(paths_tmp[i]): 
             print('No gather file for {} station in {}.\n Running extract_tdps for the dataset.'.format(checked_stations[i],project_name))
-            extract_tdps(tmp_dir,project_name,checked_stations[i],num_cores)
+            extract_tdps(tmp_dir,project_name,checked_stations[i],num_cores,tqdm)
 
         print('Found', paths_tmp[i], 'Loading...')
         gather[i] = _dump_read(paths_tmp[i])
