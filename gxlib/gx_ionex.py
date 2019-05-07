7517cb734e29e5b6efd0f8540f8af5cf9e077385
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-05-07 17:51:02 +1000

    imported libs are hidden now

--- gxlib/gx_ionex.py
+++ gxlib/gx_ionex.py
@@ -1,11 +1,11 @@
-import re
-import glob, os, sys
-import pandas as pd
-import numpy as np
-from multiprocessing import Pool
+import re as _re
+import glob as _glob, os as _os, sys as _sys
+import pandas as _pd
+import numpy as _np
+from multiprocessing import Pool as _Pool
 
-re_begin = re.compile(rb"START\sOF\s(TEC.+\n|RMS.+\n)")
-re_end =   re.compile(rb"END\sOF\s(TEC.+\n|RMS.+\n)")
+re_begin = _re.compile(rb"START\sOF\s(TEC.+\n|RMS.+\n)")
+re_end =   _re.compile(rb"END\sOF\s(TEC.+\n|RMS.+\n)")
 
 def GIM_data_extraction(file):
     # Extracting ionex data from file
@@ -13,8 +13,8 @@ def GIM_data_extraction(file):
         ionex_data = ionex.read()
         GIM_match_begin = [];GIM_match_end = []
         
-        matches_begin = re.finditer(re_begin, ionex_data)
-        matches_end   = re.finditer(re_end, ionex_data)
+        matches_begin = _re.finditer(re_begin, ionex_data)
+        matches_end   = _re.finditer(re_end, ionex_data)
 
         for matchNum, match in enumerate(matches_begin):
             GIM_match_begin.append(match.end())
@@ -23,18 +23,18 @@ def GIM_data_extraction(file):
             GIM_match_end.append(match.start()-60) #60 is the number of symbols from the end of the map to marker
             
         frame_n = len(GIM_match_begin)//2
-        GIM_boundaries_b = np.asarray(GIM_match_begin).reshape((2,frame_n))
-        GIM_boundaries_e = np.asarray(GIM_match_end).reshape((2,frame_n))
+        GIM_boundaries_b = _np.asarray(GIM_match_begin).reshape((2,frame_n))
+        GIM_boundaries_e = _np.asarray(GIM_match_end).reshape((2,frame_n))
         
-        TEC = RMS = np.ndarray((frame_n),dtype=object)
+        TEC = RMS = _np.ndarray((frame_n),dtype=object)
 
         for i in range(frame_n):
             TEC[i] = ionex_data[GIM_boundaries_b[0,i]:GIM_boundaries_e[0,i]].decode('ascii')
             RMS[i] = ionex_data[GIM_boundaries_b[1,i]:GIM_boundaries_e[1,i]].decode('ascii')
             
-        Datetime = pd.to_datetime(pd.Series(TEC).str.slice(2,36),format= '%Y %m %d %H %M %S')
+        Datetime = _pd.to_datetime(_pd.Series(TEC).str.slice(2,36),format= '%Y %m %d %H %M %S')
 
-    return pd.concat((Datetime,pd.Series(TEC),pd.Series(RMS)),axis=1)
+    return _pd.concat((Datetime,_pd.Series(TEC),_pd.Series(RMS)),axis=1)
 
 
 class ionex:
@@ -45,38 +45,38 @@ class ionex:
                 ionex_type, #='igs', #type of files
                 num_cores):
         self.ionex_type = ionex_type #add checker here
-        self.ionex_prods_dir = os.path.abspath(ionex_prods_dir)
-        self.output_path = os.path.abspath(os.path.join(self.ionex_prods_dir,os.pardir))
+        self.ionex_prods_dir = _os.path.abspath(ionex_prods_dir)
+        self.output_path = _os.path.abspath(_os.path.join(self.ionex_prods_dir,_os.pardir))
         self.num_cores = num_cores
         self.ionex_files_list = self._extended_list()
         self.years_present = self.ionex_files_list.iloc[:,0].unique()
         self.merge_lists = self._create_lists4merge(self.ionex_files_list,self.years_present)
              
     def _extended_list(self):
-        path_series = pd.Series(sorted(glob.glob(os.path.abspath(os.path.join(self.ionex_prods_dir,'*/*', self.ionex_type+'*')))))
+        path_series = _pd.Series(sorted(_glob.glob(_os.path.abspath(_os.path.join(self.ionex_prods_dir,'*/*', self.ionex_type+'*')))))
         properties_series = path_series.str.split('/',expand=True).iloc[:,-3:]
         properties_series.iloc[:,0] = properties_series.iloc[:,0].astype(int)
-        return pd.concat((properties_series,path_series),axis=1)
+        return _pd.concat((properties_series,path_series),axis=1)
     
     def _get_ionex_list(self,year,extended_list):
         if len(extended_list[extended_list.iloc[:,0]==year-1])==0 & len(extended_list[extended_list.iloc[:,0]==year+1])==0:
             list_files_out =  extended_list[extended_list.iloc[:,0]==year]
 
         if len(extended_list[extended_list.iloc[:,0]==year-1])==0:
-            list_files_out =  pd.concat((extended_list[extended_list.iloc[:,0]==year],
+            list_files_out =  _pd.concat((extended_list[extended_list.iloc[:,0]==year],
                                          extended_list[extended_list.iloc[:,0]==year+1].head(1)))
 
         if len(extended_list[extended_list.iloc[:,0]==year+1])==0:
-            list_files_out =  pd.concat((extended_list[extended_list.iloc[:,0]==year-1].tail(1),
+            list_files_out =  _pd.concat((extended_list[extended_list.iloc[:,0]==year-1].tail(1),
                                          extended_list[extended_list.iloc[:,0]==year]))
         else:
-            list_files_out =  pd.concat((extended_list[extended_list.iloc[:,0]==year-1].tail(1),
+            list_files_out =  _pd.concat((extended_list[extended_list.iloc[:,0]==year-1].tail(1),
                                          extended_list[extended_list.iloc[:,0]==year],
                                          extended_list[extended_list.iloc[:,0]==year+1].head(1)))
         return list_files_out.values
     
     def _create_lists4merge(self,ionex_files_list,years_present):
-        merge_lists = np.ndarray((len(years_present)),dtype=object)
+        merge_lists = _np.ndarray((len(years_present)),dtype=object)
         for i in range(len(years_present)):
             merge_lists[i]=self._get_ionex_list(years_present[i],ionex_files_list)
         return merge_lists
@@ -103,19 +103,19 @@ class ionex:
         with open (merge_list[0],'rb') as ionex_first:
             ionex_data_first = ionex_first.read()
 
-            match_first_epoch = re.search(regex_first_epoch, ionex_data_first)
+            match_first_epoch = _re.search(regex_first_epoch, ionex_data_first)
             
         #extracting header part from the last document ignoring aux data
         with open (merge_list[-1],'rb') as ionex_last:
             ionex_data_last  = ionex_last.read()
-            match_last_epoch = re.search(regex_first_epoch, ionex_data_last) #EPOCH OF LAST MAP (LAST FILE to continue the header as headers can have different line quantity)
+            match_last_epoch = _re.search(regex_first_epoch, ionex_data_last) #EPOCH OF LAST MAP (LAST FILE to continue the header as headers can have different line quantity)
             
-            match_num_maps_b = re.search(regex_num_maps_b, ionex_data_last)
-            match_num_maps_e = re.search(regex_num_maps_e, ionex_data_last)
+            match_num_maps_b = _re.search(regex_num_maps_b, ionex_data_last)
+            match_num_maps_e = _re.search(regex_num_maps_e, ionex_data_last)
 
-            match_aux_begin  = re.search(regex_aux_start, ionex_data_last)
-            match_aux_end    = re.search(regex_aux_end, ionex_data_last)
-            match_end_header = re.search(regex_end_header, ionex_data_last)
+            match_aux_begin  = _re.search(regex_aux_start, ionex_data_last)
+            match_aux_end    = _re.search(regex_aux_end, ionex_data_last)
+            match_end_header = _re.search(regex_end_header, ionex_data_last)
 
         return ((ionex_data_first[:match_first_epoch.end()]\
                 + ionex_data_last[match_last_epoch.end():match_num_maps_b.end()]).decode('ascii')\
@@ -125,15 +125,11 @@ class ionex:
     
     def get_ionex_data(self,merge_list):
         num_cores = self.num_cores if len(merge_list) > self.num_cores else len(merge_list)
-        chunksize = int(np.ceil(len(merge_list)/num_cores))
+        chunksize = int(_np.ceil(len(merge_list)/num_cores))
         # Collecting ionex maps from multiple files in parallel
-        try:
-            pool = Pool(num_cores)
-            GIM_data = pool.map(GIM_data_extraction, merge_list[:,3],chunksize=chunksize)
+        with _Pool(num_cores) as p:
+            GIM_data = p.map(GIM_data_extraction, merge_list[:,3],chunksize=chunksize)
             # GIM_data_extraction expects only array with filepaths
-        finally:
-            pool.close()
-            pool.join()
 
         # Need this piece of code to kill 00 values eliminating duplicates
         for i in range(len(GIM_data)-1):
@@ -141,21 +137,21 @@ class ionex:
                 GIM_data[i].drop(GIM_data[i][0].tail(1).index[0],inplace = True)
 
         # Merging all data into two arrays (TEC and RMS) 
-        data_GIM_final = pd.DataFrame()
+        data_GIM_final = _pd.DataFrame()
         for element in GIM_data:
-            data_GIM_final = pd.concat((data_GIM_final,element[[1,2]]))
+            data_GIM_final = _pd.concat((data_GIM_final,element[[1,2]]))
         # Resulting array with two columns
         return data_GIM_final.values
     
     def merge_ionex_dataset(self):
         # create dir to where the files will be written
         path = self.output_path+'/IONEX_merged/'
-        if not os.path.exists(path):
-            os.makedirs(path)
+        if not _os.path.exists(path):
+            _os.makedirs(path)
             
         EOF = '{:<60s}{}{:<9s}\n'.format(' ','END OF FILE',' ')
     
-        GIM_data=np.ndarray((len(self.years_present)),dtype=object)
+        GIM_data=_np.ndarray((len(self.years_present)),dtype=object)
         for i in range(len(self.years_present)):
             
             
