fa510cca93522ac3405bf76638be0644d9b755ec
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-05-09 00:31:29 +1000

    updated

--- gipsyx_pbs_bigf_3.2_0.1.py
+++ gipsyx_pbs_bigf_3.2_0.1.py
@@ -22,25 +22,25 @@ years_list=[2010,2011,2012,2013];num_cores = 28
 
 
 
-    kinematic_project = mGNSS_class(project_name = 'bigf',
-                                stations_list=stations_list,
-                                years_list=years_list,
-                                tree_options = trees_options.rw_otl, 
-                                num_cores=num_cores,
-                                blq_file = '/scratch/bogdanm/Products/otl/ocnld_coeff/bigf_complete.blq',
-                                rnx_dir='/scratch/bogdanm/GNSS_data/BIGF_data/daily30s',
-                                tmp_dir='/scratch/bogdanm/tmp_GipsyX/bigf_tmpX/',
-                                VMF1_dir = '/scratch/bogdanm/Products/VMF1_Products',
-                                tropNom_input = 'trop',
-                                IGS_logs_dir = '/scratch/bogdanm/GNSS_data/station_log_files/',
-                                IONEX_products = '/scratch/bogdanm/Products/IONEX_Products',
-                                rate = 300,
-                                gnss_products_dir = '/scratch/bogdanm/Products/IGS_GNSS_Products/init/es2_30h_init/',
-                                ionex_type='igs', #No ionex dir required as ionex merged products will be put into tmp directory by ionex class
-                                eterna_path='/scratch/bogdanm/Products/otl/eterna',
-                                hardisp_path = '/scratch/bogdanm/Products/otl/hardisp/hardisp',
-                                pos_s = 3.2, wetz_s=0.1,PPPtype='kinematic',tqdm=False)
-    kinematic_project.gd2e()
+kinematic_project = mGNSS_class(project_name = 'bigf',
+                            stations_list=stations_list,
+                            years_list=years_list,
+                            tree_options = trees_options.rw_otl, 
+                            num_cores=num_cores,
+                            blq_file = '/scratch/bogdanm/Products/otl/ocnld_coeff/bigf_complete.blq',
+                            rnx_dir='/scratch/bogdanm/GNSS_data/BIGF_data/daily30s',
+                            tmp_dir='/scratch/bogdanm/tmp_GipsyX/bigf_tmpX/',
+                            VMF1_dir = '/scratch/bogdanm/Products/VMF1_Products',
+                            tropNom_input = 'trop',
+                            IGS_logs_dir = '/scratch/bogdanm/GNSS_data/station_log_files/',
+                            IONEX_products = '/scratch/bogdanm/Products/IONEX_Products',
+                            rate = 300,
+                            gnss_products_dir = '/scratch/bogdanm/Products/IGS_GNSS_Products/init/es2_30h_init/',
+                            ionex_type='igs', #No ionex dir required as ionex merged products will be put into tmp directory by ionex class
+                            eterna_path='/scratch/bogdanm/Products/otl/eterna',
+                            hardisp_path = '/scratch/bogdanm/Products/otl/hardisp/hardisp',
+                            pos_s = 3.2, wetz_s=0.1,PPPtype='kinematic',tqdm=False)
+kinematic_project.gd2e()
 
 
 print('Done')
