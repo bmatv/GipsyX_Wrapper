ddb21220f1fdb75917f28dc380d83e4cebf1a8ed
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-05-10 02:34:17 +1000

    completely rewrote dr_merge

--- gxlib/gx_merge.py
+++ gxlib/gx_merge.py
@@ -1,53 +1,45 @@
 import numpy as _np
+import pandas as _pd
 import os as _os
 from subprocess import Popen as _Popen
 from multiprocessing import Pool as _Pool
 import tqdm as _tqdm
-from .gx_aux import J2000origin
+from .gx_aux import J2000origin, _dump_read
 
-
-def get_merge_table(tmp_dir,mode=None):
+def get_merge_table(tmp_dir,stations_list,mode=None):
     '''
-    Reads drInfo file and outputs merge_table for all available stations in drInfo in the same order.
-
-    Because of JPL's specific 30 hour products for GPS GipsyX is checking if centerd file is 30 hours and if so:
-    fetch one product day even with 24 hours products as ESA. To make GipsyX fetch more product days - 32 hour files 
-    should be created. In this case GipsyX will understand that 30h products are not enought and will extract three
-    days of products. This completely solves the problem with products extrapolation and gives ability of proper 
-    processing of 30 hour files as GPS as GLONASS successfully overcoming day-boundary effects.
-
+    Reads drInfo file and outputs merge_table for all available stations in drInfo in one single dataframe.
     tmp_dir is expected to have drinfo.npz file produced by get_drinfo function
     Analyses the properties of dr files and outputs classified dataset where class 3 files can be meged to 32 hours files centered on the midday.
     Currently there are no special cases for the very first and last files of the station as if merged non-symmetrically won't be centred'''
 
-
-    drinfo_file = _np.load(file=tmp_dir+'/rnx_dr/drinfo.npz',allow_pickle=True) #should overwrite to dump_zstd. This is a new security change inroduced to numpy
-    drinfo = drinfo_file['drinfo']
-    
+    tmp_dir = _os.path.abspath(tmp_dir)
+    drinfo = _dump_read(filename=tmp_dir+'/rnx_dr/drinfo.zstd') 
     modes = [None, 'GPS', 'GLONASS','GPS+GLONASS']
     if mode not in modes:
         raise ValueError("Invalid mode. Expected one of: %s" % modes)
 
 
     '''Creating classes according to record length'''
-    dr_classes = _np.ndarray((drinfo.shape[0]),dtype=object)
-    for i in range(drinfo.shape[0]):
-        if mode is None:
-            station_record = drinfo[i]
-        elif mode == 'GPS':
-            station_record = drinfo[i][drinfo[i][:,-3] == True]
-        elif mode == 'GLONASS':
-            station_record = drinfo[i][drinfo[i][:,-2] == True]
-        elif mode == 'GPS+GLONASS':
-            station_record = drinfo[i][(drinfo[i][:,-3] == True)&(drinfo[i][:,-2] == True)]
-        
-        if len(station_record) == 0:
-            raise ValueError("No data found for mode {} for station {}".format(mode,i))
+    dr_classes = []
 
-#         station_record = gps_drinfo #filtered station record
-        completeness = _np.zeros((len(station_record)),dtype=_np.int)
-        drinfo_rec_time = (station_record[:,3].astype('datetime64[h]')-station_record[:,2].astype('datetime64[h]')).astype(_np.int)
-        
+    if mode is None:
+        complete_record = drinfo #all available files will be merged. Usually this is what I start with
+    elif mode == 'GPS':
+        complete_record = drinfo[drinfo['GPS']]
+    elif mode == 'GLONASS':
+        complete_record = drinfo[drinfo['GLONASS']]
+    elif mode == 'GPS+GLONASS':
+        complete_record = drinfo[(drinfo['GPS'])&(drinfo['GLONASS'])]
+    
+    for station in stations_list:
+        station_record = complete_record[complete_record['station_name'] == station].sort_values(by='begin')
+        if station_record.shape[0] == 0:
+            raise ValueError("No data found for mode {} for station {}".format(mode,station))
+
+        drinfo_rec_time = station_record['length'].values
+
+        completeness = _np.zeros((drinfo_rec_time.shape),dtype=int)
         #-----------------------------------------------------------------------
         # Basic filtering module that uses total length of the datarecord.
         # records with more than 12 hours of data but less than 20 hours of data get 1
@@ -67,14 +59,16 @@ def get_merge_table(tmp_dir,mode=None):
         # Missing data of 1 hour is acceptable
         #-----------------------------------------------------------------------
 
+        station_start64 = station_record['begin'].values
+        station_end64 = station_record['end'].values
 
-        start_c_day=station_record[:,2].astype('datetime64[D]')
-        start_p_hour=_np.roll(station_record[:,2],1).astype('datetime64[h]')
-        start_n_hour=_np.roll(station_record[:,2],-1).astype('datetime64[h]')
+        start_c_day=station_start64.astype('datetime64[D]')
+        start_p_hour=_np.roll(station_start64,1).astype('datetime64[h]')
+        start_n_hour=_np.roll(station_start64,-1).astype('datetime64[h]')
 
         # end_c_day=station_record[:,3].astype('datetime64[D]')
-        end_p_minute=_np.roll(station_record[:,3],1).astype('datetime64[m]')
-        end_n_hour=_np.roll(station_record[:,3],-1).astype('datetime64[h]')
+        end_p_minute=_np.roll(station_end64,1).astype('datetime64[m]')
+        end_n_hour=_np.roll(station_end64,-1).astype('datetime64[h]')
 
 
         B1c1 = (start_c_day-start_p_hour <= _np.timedelta64(24,'[h]'))&(start_c_day-start_p_hour >= _np.timedelta64(3,'[h]'))
@@ -87,46 +81,53 @@ def get_merge_table(tmp_dir,mode=None):
         
          
         completeness[B1c1 & B1c2 & B2c1 & B2c2 & (completeness==2)] = 3
-        dr_classes[i] = _np.column_stack((completeness,
-                                            station_record[:,2], #record start time
-                                            station_record[:,3], #record end time
-                                            _np.roll(station_record[:,-1],1), #filename previous
-                                            station_record[:,-1], #filename
-                                            _np.roll(station_record[:,-1],-1), #filename next
-                                          
-                                        ))
-        #     return dataRecordInfo
-    return dr_classes
+
+        tmp_df = station_record.copy()
+        tmp_df['completeness'] = completeness
+
+        tmp_df['path_prev'] =  tmp_dir + _np.roll(station_record['path'].values,1) 
+        tmp_df['path'] =  tmp_dir + station_record['path']
+        tmp_df['path_next'] =  tmp_dir +_np.roll(station_record['path'].values,-1)
+
+        dr_classes.append(tmp_df)
+    return _pd.concat(dr_classes,axis=0)
 
 def _merge(merge_set):
-    '''Expects a merge set of 3 files [class,time,file0,file1,file2]. Merges all files into file1_20h. file1 must be a class 3 file
+    '''Expects a merge set of 3 files [merge_start, merge_end,file_prev,file,file_next]. Merges all files into file120h. file1 must be a class 3 file
     Constructs 32h files with drMerge.py.
     Sample input:
     drMerge.py -i isba0940.15o.dr ohln0940.15o.dr -start 2015-04-04 00:00:00 -end 2015-04-04 04:00:00
     '''
-    if not _os.path.isfile((merge_set[4])[:-6]+'.dr.gz.30h'):
-        #Computing time boundaries of the merge. merge_set[1] is file begin time
-        merge_begin = ((merge_set[1].astype('datetime64[D]') - _np.timedelta64( 3,'[h]'))-J2000origin).astype('timedelta64[s]').astype(int).astype(str)
-        merge_end =   ((merge_set[1].astype('datetime64[D]') + _np.timedelta64(27,'[h]'))-J2000origin).astype('timedelta64[s]').astype(int).astype(str)
+    #Computing time boundaries of the merge. merge_set[1] is file begin time
+    drMerge_proc = _Popen(['drMerge', str(merge_set[0]), str(merge_set[1]), _os.path.basename(merge_set[3])+'.30h',\
+                merge_set[2], merge_set[3], merge_set[4] ],\
+                cwd=_os.path.dirname(merge_set[3]))
+    drMerge_proc.wait()
 
-        drMerge_proc = _Popen(['drMerge', merge_begin, merge_end,\
-                    _os.path.basename(merge_set[4])[:-6]+'.dr.gz.30h',\
-                    merge_set[3], merge_set[4], merge_set[5] ],\
-                    cwd=_os.path.dirname(merge_set[4]))
-        drMerge_proc.wait()
-
-def dr_merge(merge_table,stations_list,num_cores,tqdm):
-    '''merge_table is the output of get_merge_table()'''
+def dr_merge(merge_table,num_cores,tqdm):
+    '''merge_table is the output of get_merge_table(). Merges all that is of class 3 as merge_table stores only files that are actual'''
     num_cores = int(num_cores) #safety precaution if str value is specified
+    df_class3 =  merge_table[['begin','path_prev','path','path_next']][merge_table['completeness']==3].copy()
+    
+    df_class3['merge_begin'] = (df_class3['begin']  - _np.timedelta64( 3,'[h]') -J2000origin).astype('timedelta64[s]').astype(int)
+    df_class3['merge_end'] = (df_class3['begin']  + _np.timedelta64( 27,'[h]') -J2000origin).astype('timedelta64[s]').astype(int)
+    
+    merge_table_class3 = df_class3[['merge_begin','merge_end','path_prev','path','path_next']]
 
+    
+    # check if merged version already exists
+    ifexists = _np.zeros((merge_table_class3.shape[0]))
+    merged_paths = merge_table_class3['path']+'.30h'
+    
+    for i in range(merged_paths.shape[0]):
+        ifexists[i] = _os.path.isfile(merged_paths.values[i])
+    ifexists = ifexists.astype(bool)
 
-    for i in range(len(stations_list)):
-        num_cores = num_cores if len(merge_table[i]) > num_cores else len(merge_table[i])
-        merge_table_class3 = merge_table[i][merge_table[i][:,0]==3]
+    merge_table_class3_run = merge_table_class3[~ifexists]
+    num_cores = num_cores if merge_table_class3_run.shape[0] > num_cores else merge_table_class3_run.shape[0]
+    
+    print('Number of files to merge:', merge_table_class3_run.shape[0],'| Adj. num_cores:', num_cores)
 
-        print(stations_list[i],'station binary files merging (class_3 only)...')
-        print ('Number of files to process:', len(merge_table_class3),'| Adj. num_cores:', num_cores)
-
-        with _Pool(processes = num_cores) as p:
-            if tqdm: list(_tqdm.tqdm_notebook(p.imap(_merge, merge_table_class3), total=merge_table_class3.shape[0]))
-            else: p.map(_merge, merge_table_class3)
+    with _Pool(processes = num_cores) as p:
+        if tqdm: list(_tqdm.tqdm_notebook(p.imap(_merge, merge_table_class3_run.values), total=merge_table_class3_run.shape[0]))
+        else: p.map(_merge, merge_table_class3_run.values)
