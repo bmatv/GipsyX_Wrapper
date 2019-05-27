c17db0596252ed1003e3460142296cab25fae417
Bogdan Matviichuk <Bogdan.Matviichuk@utas.edu.au>
2019-05-28 00:00:26 +1000

    added clk adkustment part to tree

--- trees_options.py
+++ trees_options.py
@@ -9,6 +9,9 @@ penna_k_randomwalk = [[
         ['GRN_STATION_CLK_WHITE:Trop:GradNorth:StochasticAdj', '1.0 5e-6 $GLOBAL_DATA_RATE RANDOMWALK'],
         ['GRN_STATION_CLK_WHITE:Trop:GradEast:StochasticAdj', '1.0 5e-6 $GLOBAL_DATA_RATE RANDOMWALK'],
 
+        ['GRN_STATION_CLK_WHITE:Clk:Bias', '0.0'],
+        ['GRN_STATION_CLK_WHITE:Clk:Bias:StochasticAdj', '3.0e8 3.0e8 $GLOBAL_DATA_RATE WHITENOISE'],
+
         # Station properties. e.g. CAMB. So we will get wetz etc in the output. Highly important!!!
         ['Station', '`cat $STATIONLIST`'+'\nStation `staDb2TreeIn.py -s $STATIONLIST -y2kSecs $GLOBAL_EPOCH -d $STA_DB`'],
         # VMF1dataDir path  
