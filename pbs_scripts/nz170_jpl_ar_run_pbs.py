a90f06bfa833e664b805a4f899891fd82a0c182e
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2019-11-13 19:14:20 +1100

    added CHTI RAUL so convert and drinfo

--- pbs_scripts/nz170_jpl_ar_run_pbs.py
+++ pbs_scripts/nz170_jpl_ar_run_pbs.py
@@ -15,30 +15,30 @@ from gxlib.gx_trees import gen_trees
 
 #parameters to change-----------------------------------------------------------------------------------------------
 
-project_name = 'nz168_jpl_cm'
+project_name = 'nz170_jpl_cm'
 ionex_type='jpl' #igs ionex map igsg2260.15i is missing data
 # gnss_products_dir = '/scratch/bogdanm/Products/JPL_GPS_Products_IGb08/source/Final' #missing files for 2018
 gnss_products_dir = '/scratch/bogdanm/Products/JPL_GNSS_Products/source/Final'
 
 '''Execution part here
-GPS continuous stations 2013-2019 (NOW) (168 stations)''' 
-stations_list= ['2406', 'AHTI', 'AKTO', 'ANAU', 'ARTA', 'AUCK', 'AUKT', 'AVLN', 'BHST', 'BIRF',
-                'BLUF', 'BNET', 'BTHL', 'CAST', 'CKID', 'CLIM', 'CMBL', 'CNCL', 'CNST', 'CORM',
-                'DNVK', 'DUND', 'DUNT', 'DURV', 'FRTN', 'GISB', 'GLDB', 'GNBK', 'GRNG', 'HAAS',
-                'HAMT', 'HANA', 'HAST', 'HIKB', 'HOKI', 'HOLD', 'HORN', 'KAHU', 'KAIK', 'KAPT',
-                'KARA', 'KAWK', 'KERE', 'KOKO', 'KORO', 'KTIA', 'KUTA', 'LDRZ', 'LEVN', 'LEXA',
-                'LEYL', 'LKTA', 'LYTT', 'MAHA', 'MAHI', 'MAHO', 'MAKO', 'MANG', 'MATW', 'MAVL',
-                'MCNL', 'METH', 'MING', 'MKNO', 'MNHR', 'MQZG', 'MTBL', 'MTJO', 'MTPR', 'MTQN',
-                'NLSN', 'NMAI', 'NPLY', 'NRRD', 'NRSW', 'OHIN', 'OKOH', 'OPTK', 'OROA', 'OTAK',
-                'OUSD', 'PAEK', 'PAKI', 'PALI', 'PARI', 'PARW', 'PAWA', 'PILK', 'PKNO', 'PNUI',
-                'PORA', 'PRTU', 'PTOI', 'PUKE', 'PYGR', 'QUAR', 'RAHI', 'RAKW', 'RAUM', 'RAWI',
-                'RDLV', 'RGAR', 'RGAW', 'RGHD', 'RGHL', 'RGHR', 'RGKA', 'RGKW', 'RGLI', 'RGMK',
-                'RGMT', 'RGON', 'RGOP', 'RGRE', 'RGRR', 'RGTA', 'RGUT', 'RGWI', 'RGWV', 'RIPA',
-                'SNST', 'TAKP', 'TAUP', 'TAUW', 'TEMA', 'TGHO', 'TGHR', 'TGOH', 'TGRA', 'TGRI',
-                'TGTK', 'TGWH', 'THAP', 'TINT', 'TKAR', 'TORY', 'TRAV', 'TRNG', 'TRWH', 'TURI',
-                'VEXA', 'VGFW', 'VGKR', 'VGMO', 'VGMT', 'VGOB', 'VGOT', 'VGPK', 'VGTR', 'VGTS',
-                'VGWH', 'VGWN', 'VGWT', 'WAHU', 'WAIM', 'WAKA', 'WANG', 'WARK', 'WEST', 'WGTN',
-                'WGTT', 'WHKT', 'WHNG', 'WHVR', 'WITH', 'WMAT', 'WPAW', 'WPUK']
+GPS continuous stations 2013-2019 (NOW) (170 stations)''' 
+stations_list= ['2406','AHTI','AKTO','ANAU','ARTA','AUCK','AUKT','AVLN','BHST','BIRF',
+                'BLUF','BNET','BTHL','CAST','CHTI','CKID','CLIM','CMBL','CNCL','CNST',
+                'CORM','DNVK','DUND','DUNT','DURV','FRTN','GISB','GLDB','GNBK','GRNG',
+                'HAAS','HAMT','HANA','HAST','HIKB','HOKI','HOLD','HORN','KAHU','KAIK',
+                'KAPT','KARA','KAWK','KERE','KOKO','KORO','KTIA','KUTA','LDRZ','LEVN',
+                'LEXA','LEYL','LKTA','LYTT','MAHA','MAHI','MAHO','MAKO','MANG','MATW',
+                'MAVL','MCNL','METH','MING','MKNO','MNHR','MQZG','MTBL','MTJO','MTPR',
+                'MTQN','NLSN','NMAI','NPLY','NRRD','NRSW','OHIN','OKOH','OPTK','OROA',
+                'OTAK','OUSD','PAEK','PAKI','PALI','PARI','PARW','PAWA','PILK','PKNO',
+                'PNUI','PORA','PRTU','PTOI','PUKE','PYGR','QUAR','RAHI','RAKW','RAUL',
+                'RAUM','RAWI','RDLV','RGAR','RGAW','RGHD','RGHL','RGHR','RGKA','RGKW',
+                'RGLI','RGMK','RGMT','RGON','RGOP','RGRE','RGRR','RGTA','RGUT','RGWI',
+                'RGWV','RIPA','SNST','TAKP','TAUP','TAUW','TEMA','TGHO','TGHR','TGOH',
+                'TGRA','TGRI','TGTK','TGWH','THAP','TINT','TKAR','TORY','TRAV','TRNG',
+                'TRWH','TURI','VEXA','VGFW','VGKR','VGMO','VGMT','VGOB','VGOT','VGPK',
+                'VGTR','VGTS','VGWH','VGWN','VGWT','WAHU','WAIM','WAKA','WANG','WARK',
+                'WEST','WGTN','WGTT','WHKT','WHNG','WHVR','WITH','WMAT','WPAW','WPUK']
 #'SCTB' station removed as it is in Anatarctica and almost no OTL
 years_list=[2013,2014,2015,2016,2017,2018,2019];num_cores = 28
 num_nodes = 20 #default is 10 . nz gd2e shows full load of 20 nodes
@@ -82,7 +82,7 @@ for i in range(len(stations_list_arrays)):
                     staDb_path = staDb_path,years_list=years_list,num_cores=num_cores,tmp_dir=tmp_dir,project_name=project_name,IGS_logs_dir=IGS_logs_dir,blq_file=blq_file,
                     VMF1_dir = VMF1_dir,pos_s = pos_s,wetz_s = wetz_s,PPPtype = PPPtype,ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
                     gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,hatanaka=hatanaka,tree_options = tree_options_code,tqdm=False,
-                    command='get_drInfo()')
+                    command='rnx2dr();kinematic_project.get_drInfo()')
     qsub_python_code(code,name='{}{}'.format(project_name,str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = '/scratch/bogdanm/pbs')
 
 #'gps.gd2e();kinematic_project.gps.envs(dump=True)'
