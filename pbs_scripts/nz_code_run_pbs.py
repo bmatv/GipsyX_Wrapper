4cbd4d153485bf97146c424ef6d80eb551797626
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2019-11-26 14:43:32 +1100

    expanding code dataset to 67 from 54

--- pbs_scripts/nz_code_run_pbs.py
+++ pbs_scripts/nz_code_run_pbs.py
@@ -22,25 +22,28 @@ gnss_products_dir = '/scratch/bogdanm/Products/CODE/init/com' #we should use COD
 
 
 '''Execution part here''' 
-stations_list= ['ANAU', 'AUCK', 'BLUF', 'CHTI', 'CORM', 'DNVK', 'DUND', 'DUNT', 'FRTN',
-                'GISB', 'GLDB', 'HAAS', 'HAMT', 'HAST', 'HIKB', 'HOKI', 'KAIK', 'KTIA',
-                'LEXA', 'LEYL', 'LKTA', 'MAHO', 'MAKO', 'MAVL', 'METH', 'MKNO', 'MNHR',
-                'MQZG', 'MTJO', 'NLSN', 'NPLY', 'NRSW', 'OROA', 'PKNO', 'RAHI', 'RAKW',
-                'RAUM', 'RGHL', 'RGKW', 'RGMT', 'TAUP', 'TAUW', 'TGRI', 'TRNG', 'TRWH',
-                'VGMT', 'WAIM', 'WANG', 'WARK', 'WEST', 'WGTN', 'WHKT', 'WHNG', 'WITH']
+stations_list= ['2406', 'ANAU', 'AUCK', 'BLUF', 'BTHL', 'CHTI', 'CORM', 'DNVK',
+                'DUND', 'DUNT', 'FRTN', 'GISB', 'GLDB', 'HAAS', 'HAMT', 'HANA',
+                'HAST', 'HIKB', 'HOKI', 'KAIK', 'KTIA', 'LDRZ', 'LEXA', 'LEYL',
+                'LKTA', 'LYTT', 'MAHO', 'MAKO', 'MAVL', 'METH', 'MKNO', 'MNHR',
+                'MQZG', 'MTJO', 'NLSN', 'NPLY', 'NRSW', 'OKOH', 'OROA', 'PAKI',
+                'PKNO', 'PYGR', 'RAHI', 'RAKW', 'RAUL', 'RAUM', 'RGHL', 'RGKW',
+                'RGMT', 'RGWI', 'TAUP', 'TAUW', 'TGRI', 'TRNG', 'TRWH', 'TURI',
+                'VGMT', 'VGWT', 'WAIM', 'WANG', 'WARK', 'WEST', 'WGTN', 'WHKT',
+                'WHNG', 'WHVR', 'WITH']
 #'SCTB' station removed as it is in Anatarctica and almost no OTL
-years_list=[2014,2015,2016,2017,2018];num_cores = 28
+years_list=[2013,2014,2015,2016,2017,2018];num_cores = 28
 num_nodes = 20 #default is 10 . nz gd2e shows full load of 20 nodes
 if num_nodes > len(stations_list): num_nodes = len(stations_list) #in case staions num is less than num_nodes => num_nodes = stations num
 #-------------------------------------------------------------------------------------------------------------------
 
 #We need to generate unique staDb with all the stations
 tmp_dir='/scratch/bogdanm/tmp_GipsyX/nz_tmpX/'
-rnx_dir='/scratch/bogdanm/GNSS_data/geonet_nz'
-hatanaka=True
+rnx_dir='/scratch/bogdanm/GNSS_data/geonet_nz_ogz'
+hatanaka=False
 IGS_logs_dir = '/scratch/bogdanm/GNSS_data/station_log_files/nz_logs'
 tree_options = trees_options.rw_otl
-blq_file = '/scratch/bogdanm/Products/otl/ocnld_coeff/FES2004_GBe.blq'
+blq_file = '/scratch/bogdanm/Products/otl/ocnld_coeff/nz/NZ183_FES2004_GBe_CE.blq'
 ElMin = 7
 pos_s = 3.2
 wetz_s=0.1
@@ -72,9 +75,9 @@ for i in range(len(stations_list_arrays)):
                     staDb_path = staDb_path,years_list=years_list,num_cores=num_cores,tmp_dir=tmp_dir,project_name=project_name,IGS_logs_dir=IGS_logs_dir,blq_file=blq_file,
                     VMF1_dir = VMF1_dir,pos_s = pos_s,wetz_s = wetz_s,PPPtype = PPPtype,ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
                     gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,hatanaka=hatanaka,tree_options = tree_options_code,tqdm=False,
-                    command='dr_merge();kinematic_project.gd2e();kinematic_project.gather_mGNSS()')
+                    command='rnx2dr()')
     qsub_python_code(code,name='{}{}'.format(project_name,str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = '/scratch/bogdanm/pbs')
-
+#'dr_merge();kinematic_project.gd2e();kinematic_project.gather_mGNSS()'
 #;kinematic_project.dr_merge();kinematic_project.gd2e();kinematic_project.gather_mGNSS()
 #.gather_drInfo() #single node
 #gen_tropNom can not be run rhis way as we need all the stations to be present
