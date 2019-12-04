afa677b9a9136f34ec3816224aed67683ffc7fa6
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2019-12-04 13:03:36 +1100

    elmin added into qsub name

--- pbs_scripts/nz_esa_run_pbs_el20.py
+++ pbs_scripts/nz_esa_run_pbs_el20.py
@@ -76,7 +76,7 @@ for i in range(len(stations_list_arrays)):
                     VMF1_dir = VMF1_dir,pos_s = pos_s,wetz_s = wetz_s,PPPtype = PPPtype,ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
                     gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,hatanaka=hatanaka,tree_options = tree_options_code,tqdm=False,
                     command='gd2e();kinematic_project.gather_mGNSS()')
-    qsub_python_code(code,name='{}{}'.format(project_name,str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = '/scratch/bogdanm/pbs')
+    sub_python_code(code,name='{}{}{}'.format(project_name,str(ElMin) if ElMin != 7 else '',str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = '/scratch/bogdanm/pbs')
 #'dr_merge();kinematic_project.gd2e();kinematic_project.gather_mGNSS()'
 #;kinematic_project.dr_merge();kinematic_project.gd2e();kinematic_project.gather_mGNSS()
 #.gather_drInfo() #single node
