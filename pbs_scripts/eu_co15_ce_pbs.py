ba875cf83dabf5ed0380d9a1957809a58806028d
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2019-12-30 16:31:36 +1100

    updated co2015 pbs + cm

--- pbs_scripts/eu_co15_ce_pbs.py
+++ pbs_scripts/eu_co15_ce_pbs.py
@@ -1,4 +1,5 @@
-import numpy as np
+import os as _os
+import numpy as _np
 import sys as _sys
 GIPSY_WRAP_PATH="/scratch/bogdanm/gipsyx/GipsyX_Wrapper"
 if GIPSY_WRAP_PATH not in _sys.path:
@@ -21,7 +22,7 @@ stations_list= ['LERI','PADT', 'PMTH', 'PRAE', 'APPL', 'EXMO',
                 'ANLX','HERT','LOFT','WEAR','CAMO','BRAE','BRST','ZIM2']
 #'SCTB' station removed as it is in Anatarctica and almost no OTL
 years_list=[2010,2011,2012,2013];num_cores = 28
-num_nodes = 10
+num_nodes = 21
 if num_nodes > len(stations_list): num_nodes = len(stations_list) #in case staions num is less than num_nodes => num_nodes = stations num
 #-------------------------------------------------------------------------------------------------------------------
 
@@ -46,23 +47,22 @@ eterna_path='/scratch/bogdanm/Products/otl/eterna'
 hardisp_path = '/scratch/bogdanm/Products/otl/hardisp/hardisp'
 tree_options_code = 'trees_options.rw_otl'
 tqdm=False
+ElDepWeight = 'SqrtSin'
 
-project_name_construct = _project_name_construct(project_name,PPPtype,pos_s,wetz_s,tropNom_input,ElMin)
+pbs_base = _os.path.join('/scratch/bogdanm/pbs',project_name) #break down by project folders as gets slow on hpc with multiple files
+project_name_construct = _project_name_construct(project_name,PPPtype,pos_s,wetz_s,tropNom_input,ElMin,ambres)
 #generating tree files that won't be overwritten as crc32 will be the same
-gen_trees(  ionex_type=ionex_type,tmp_dir=tmp_dir,tree_options=tree_options,blq_file=blq_file,mode = 'GPS+GLONASS',
+gen_trees(  ionex_type=ionex_type,tmp_dir=tmp_dir,tree_options=tree_options,blq_file=blq_file,mode = 'GPS+GLONASS',ElDepWeight=ElDepWeight,
             ElMin = ElMin,pos_s = pos_s,wetz_s = wetz_s,PPPtype = PPPtype,years_list=years_list,cache_path = cache_path,
             VMF1_dir = VMF1_dir,project_name = project_name_construct,static_clk = static_clk,ambres = ambres)#the GNSS_class single project name
 
 staDb_path = gen_staDb(tmp_dir = tmp_dir, project_name = project_name, stations_list = stations_list, IGS_logs_dir = IGS_logs_dir)
-stations_list_arrays = np.array_split(stations_list,num_nodes)
+stations_list_arrays = _np.array_split(stations_list,num_nodes)
 for i in range(len(stations_list_arrays)):
-    code = gen_code(stations_list = list(stations_list_arrays[i]), cache_path = cache_path,tropNom_input=tropNom_input, ambres = ambres,ElMin=ElMin,
+    code = gen_code(stations_list = list(stations_list_arrays[i]), cache_path = cache_path,tropNom_input=tropNom_input, ambres = ambres,ElMin=ElMin,ElDepWeight=ElDepWeight,
                     staDb_path = staDb_path,years_list=years_list,num_cores=num_cores,tmp_dir=tmp_dir,project_name=project_name,IGS_logs_dir=IGS_logs_dir,blq_file=blq_file,
                     VMF1_dir = VMF1_dir,pos_s = pos_s,wetz_s = wetz_s,PPPtype = PPPtype,ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
-                    gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,tree_options = tree_options_code,tqdm=False,
-                    command='dr_merge();kinematic_project.gd2e();kinematic_project.gather_mGNSS()')
-    qsub_python_code(code,name='{}{}'.format(project_name,str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = '/scratch/bogdanm/pbs')
+                    gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,hatanaka=hatanaka,tree_options = tree_options_code,tqdm=False,
+                    command='gd2e();kinematic_project.gather_mGNSS()')
 
-#gen_tropNom can not be run rhis way as we need all the stations to be present
-# kinematic_project.get_drInfo()   .gather_drInfo()
-# 'gd2e()' was executed!!! ALL OK
+    qsub_python_code(code,name='{}{}{}'.format(project_name,str(ElMin) if ElMin != 7 else '',str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = pbs_base)
