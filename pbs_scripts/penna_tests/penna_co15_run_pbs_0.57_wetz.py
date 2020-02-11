763acfe7fb9a67f23d1667e78f76475067ef208f
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2020-02-11 15:32:06 +1100

    wetz pbs co2015 updated

--- pbs_scripts/penna_tests/penna_co15_run_pbs_0.57_wetz.py
+++ pbs_scripts/penna_tests/penna_co15_run_pbs_0.57_wetz.py
@@ -1,19 +1,21 @@
-import numpy as np
-import sys as _sys
+###
+import sys as _sys, os as _os
+import numpy as _np
+
 GIPSY_WRAP_PATH="/scratch/bogdanm/gipsyx/GipsyX_Wrapper"
 if GIPSY_WRAP_PATH not in _sys.path:
     _sys.path.insert(0,GIPSY_WRAP_PATH)
+
 import trees_options
-from gxlib.gx_aux import gen_staDb, _project_name_construct
-from gxlib.gx_trees import gen_trees
+from gxlib.gx_aux import _project_name_construct, gen_staDb, prepare_dir_struct_dr, prepare_dir_struct_gathers
 from gxlib.gx_pbs import gen_code, qsub_python_code
+from gxlib.gx_trees import gen_trees
 
 #parameters to change-----------------------------------------------------------------------------------------------
 
 project_name = 'penna_cod_ce'
 ionex_type='cod' #igs ionex map igsg2260.15i is missing data
-gnss_products_dir = '/scratch/bogdanm/Products/CODE/init/REPRO_2015' #we should use COD MGEX, ESA and GFZ later
-
+gnss_products_dir = '/scratch/bogdanm/Products/CODE/init/co2015' #we should use COD MGEX, ESA and GFZ later
 
 '''Execution part here''' 
 stations_list= ['CAMO']
@@ -29,6 +31,8 @@ if num_nodes > len(penna_wetz_list): num_nodes = len(penna_wetz_list) #in case p
 #We need to generate unique staDb with all the stations
 tmp_dir='/scratch/bogdanm/tmp_GipsyX/bigf_tmpX/'
 rnx_dir='/scratch/bogdanm/GNSS_data/BIGF_data/daily30s'
+hatanaka=True
+cddis=False
 IGS_logs_dir = '/scratch/bogdanm/GNSS_data/station_log_files/bigf_igs_logs'
 tree_options = trees_options.rw_otl
 blq_file = '/scratch/bogdanm/Products/otl/ocnld_coeff/FES2004_GBe.blq'
@@ -47,20 +51,32 @@ tree_options_code = 'trees_options.rw_otl'
 tqdm=False
 ElDepWeight = 'SqrtSin'
 
+####
 staDb_path = gen_staDb(tmp_dir = tmp_dir, project_name = project_name, stations_list = stations_list, IGS_logs_dir = IGS_logs_dir)
-
+pbs_base = _os.path.join('/scratch/bogdanm/pbs',project_name) #break down by project folders as gets slow on hpc with multiple files
 for i in range(len(penna_wetz_list)):
+    prepare_dir_struct_dr(begin_year=_np.min(years_list), end_year = _np.max(years_list),tmp_dir=tmp_dir) #prepare dir struct for dr files
+    project_name_construct = _project_name_construct(project_name=project_name,PPPtype=PPPtype,pos_s=pos_s,wetz_s=wetz_s,tropNom_input=tropNom_input,ElMin=ElMin,ambres=ambres)
+    prepare_dir_struct_gathers(tmp_dir=tmp_dir,project_name=project_name_construct)
+    wetz_s = penna_wetz_list[i]
     code = gen_code(stations_list = stations_list, cache_path = cache_path,tropNom_input=tropNom_input, ambres = ambres,ElMin=ElMin, ElDepWeight=ElDepWeight,
                     staDb_path = staDb_path,years_list=years_list,num_cores=num_cores,tmp_dir=tmp_dir,project_name=project_name,IGS_logs_dir=IGS_logs_dir,blq_file=blq_file,VMF1_dir = VMF1_dir,
-                    
+                    hatanaka=hatanaka,cddis=cddis,
                     pos_s = pos_s,
-                    wetz_s = penna_wetz_list[i],
+                    wetz_s = wetz_s,
 
                     PPPtype = PPPtype,ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
                     gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,tree_options = tree_options_code,tqdm=False,
                     command='gd2e();kinematic_project.gather_mGNSS()')
-    qsub_python_code(code,name='{}{}'.format(project_name,str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = '/scratch/bogdanm/pbs', walltime = '02:00:00')
+    qsub_python_code(code,name='{}_057_{}'.format(project_name,str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = pbs_base, walltime = '02:00:00')
 
-#gen_tropNom can not be run rhis way as we need all the stations to be present
-# kinematic_project.get_drInfo()   .gather_drInfo()
-# 'gd2e()' was executed!!! ALL OK
+#single static run
+code = gen_code(stations_list = stations_list, cache_path = cache_path,tropNom_input=tropNom_input, ambres = ambres,ElMin=ElMin, ElDepWeight=ElDepWeight,
+                    staDb_path = staDb_path,years_list=years_list,num_cores=num_cores,tmp_dir=tmp_dir,project_name=project_name,IGS_logs_dir=IGS_logs_dir,blq_file=blq_file,VMF1_dir = VMF1_dir,
+                    hatanaka=hatanaka,cddis=cddis,
+                    pos_s = 0,
+                    wetz_s = 0.05,
+                    PPPtype = 'static',ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
+                    gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,tree_options = tree_options_code,tqdm=False,
+                    command='gd2e();kinematic_project.gather_mGNSS()')
+qsub_python_code(code,name='{}_static'.format(project_name),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = pbs_base, walltime = '02:00:00')
