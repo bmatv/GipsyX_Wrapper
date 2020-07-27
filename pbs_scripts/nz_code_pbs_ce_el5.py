9fbac573c365303f5995a4547236c4618c08af44
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2020-07-27 15:27:19 +1000

    updated cutoff files for geonet

--- pbs_scripts/nz_code_pbs_ce_el5.py
+++ pbs_scripts/nz_code_pbs_ce_el5.py
@@ -1,15 +1,15 @@
-import numpy as _np
-import os as _os
 import sys as _sys
+import os as _os
+import numpy as _np
+
 GIPSY_WRAP_PATH="/scratch/bogdanm/gipsyx/GipsyX_Wrapper"
 if GIPSY_WRAP_PATH not in _sys.path:
     _sys.path.insert(0,GIPSY_WRAP_PATH)
-import trees_options
-from gxlib.gx_aux import gen_staDb, _project_name_construct
-from gxlib.gx_trees import gen_trees
-from gxlib.gx_pbs import gen_code, qsub_python_code
 
-                           
+import trees_options
+from gxlib.gx_aux import _project_name_construct, gen_staDb, prepare_dir_struct_dr, prepare_dir_struct_gathers
+from gxlib.gx_pbs import gen_code, qsub_python_code
+from gxlib.gx_trees import gen_trees
 
 #parameters to change-----------------------------------------------------------------------------------------------
 
@@ -18,19 +18,29 @@ ionex_type='cod' #igs ionex map igsg2260.15i is missing data
 gnss_products_dir = '/scratch/bogdanm/Products/CODE/init/com' #we should use COD MGEX, ESA and GFZ later
 
 '''Execution part here''' 
-stations_list= ['2406', 'ANAU', 'AUCK', 'BLUF', 'BTHL', 'CAST', 'CHTI', 'CMBL',
-                'CORM', 'DNVK', 'DUND', 'DUNT', 'FRTN', 'GISB', 'GLDB', 'HAAS',
-                'HAMT', 'HANA', 'HAST', 'HIKB', 'HOKI', 'KAIK', 'KTIA', 'LDRZ',
-                'LEXA', 'LEYL', 'LKTA', 'LYTT', 'MAHO', 'MAKO', 'MAVL', 'METH',
-                'MKNO', 'MNHR', 'MQZG', 'MTJO', 'NLSN', 'NPLY', 'NRSW', 'OKOH',
-                'OROA', 'PAEK', 'PAKI', 'PKNO', 'PYGR', 'QUAR', 'RAHI', 'RAKW',
-                'RAUL', 'RAUM', 'RGHL', 'RGKW', 'RGLI', 'RGMT', 'RGRE', 'RGRR',
-                'RGWI', 'TAUP', 'TAUW', 'TGRI', 'TRNG', 'TRWH', 'TURI', 'VGMT',
-                'VGTM', 'VGWT', 'WAIM', 'WAKA', 'WANG', 'WARK', 'WEST', 'WGTN',
-                'WHKT', 'WHNG', 'WHVR', 'WITH', 'WMAT']
+#131 GLONASS stations active constantly after 2016-06-01 (131)
+stations_list= ['2406', 'AHTI', 'AKTO', 'ANAU', 'ARTA', 'AUCK', 'BHST', 'BIRF',
+        'BLUF', 'BTHL', 'CAST', 'CHTI', 'CLIM', 'CMBL', 'CNCL', 'CNST',
+        'CORM', 'DNVK', 'DUND', 'DUNT', 'FRTN', 'GISB', 'GLDB', 'GNBK',
+        'GRNG', 'HAAS', 'HAMT', 'HANA', 'HANM', 'HAST', 'HIKB', 'HOKI',
+        'KAIK', 'KARA', 'KAWK', 'KOKO', 'KORO', 'KTIA', 'LDRZ', 'LEVN',
+        'LEXA', 'LEYL', 'LKTA', 'LYTT', 'MAHO', 'MAKO', 'MANG', 'MATW',
+        'MAVL', 'METH', 'MING', 'MKNO', 'MNHR', 'MQZG', 'MRBL', 'MTBL',
+        'MTJO', 'MTPR', 'NLSN', 'NPLY', 'NRRD', 'NRSW', 'OHIN', 'OKOH',
+        'OPTK', 'OROA', 'OTAK', 'PAEK', 'PAKI', 'PALI', 'PARW', 'PAWA',
+        'PGKH', 'PGNE', 'PILK', 'PKNO', 'PORA', 'PRTU', 'PTOI', 'PUKE',
+        'PYGR', 'QUAR', 'RAHI', 'RAKW', 'RAUL', 'RAUM', 'RAWI', 'RDLV',
+        'RGAW', 'RGHL', 'RGKW', 'RGLI', 'RGMK', 'RGMT', 'RGRE', 'RGRR',
+        'RGWC', 'RGWI', 'RIPA', 'SNST', 'TAKP', 'TAUP', 'TAUW', 'TGRI',
+        'TKAR', 'TKHL', 'TRNG', 'TRWH', 'TURI', 'VEXA', 'VGET', 'VGMT',
+        'VGNG', 'VGNT', 'VGOB', 'VGTM', 'VGTR', 'VGWT', 'WAIM', 'WAKA',
+        'WANG', 'WARK', 'WEST', 'WGTN', 'WHKT', 'WHNG', 'WHVR', 'WITH',
+        'WMAT', 'WRPA'] #, 'YALD' removed for now
+
+
 #'SCTB' station removed as it is in Anatarctica and almost no OTL. 77 stations GPS+GLO 2014.0-2019.0
-years_list=[2014,2015,2016,2017,2018];num_cores = 28
-num_nodes = 20 #default is 10 . nz gd2e shows full load of 20 nodes
+years_list=[2013,2014,2015,2016,2017,2018,2019,2020];num_cores = 28
+num_nodes = 30 #default is 10 . nz gd2e shows full load of 20 nodes
 if num_nodes > len(stations_list): num_nodes = len(stations_list) #in case staions num is less than num_nodes => num_nodes = stations num
 #-------------------------------------------------------------------------------------------------------------------
 
@@ -39,9 +49,11 @@ tmp_dir='/scratch/bogdanm/tmp_GipsyX/nz_tmpX/'
 rnx_dir='/scratch/bogdanm/GNSS_data/geonet_nz_ogz'
 hatanaka=False
 IGS_logs_dir = '/scratch/bogdanm/GNSS_data/station_log_files/nz_logs'
+
+cddis=False
 tree_options = trees_options.rw_otl
 blq_file = '/scratch/bogdanm/Products/otl/ocnld_coeff/nz/NZ183_FES2004_GBe_CE.blq'
-ElMin = 20
+ElMin = 5
 pos_s = 3.2
 wetz_s=0.1
 PPPtype='kinematic'
@@ -58,6 +70,10 @@ tree_options_code = 'trees_options.rw_otl'
 tqdm=False
 ElDepWeight = 'SqrtSin'
 
+prepare_dir_struct_dr(begin_year=_np.min(years_list), end_year = _np.max(years_list),tmp_dir=tmp_dir) #prepare dir struct for dr files
+project_name_construct = _project_name_construct(project_name=project_name,PPPtype=PPPtype,pos_s=pos_s,wetz_s=wetz_s,tropNom_input=tropNom_input,ElMin=ElMin,ambres=ambres)
+prepare_dir_struct_gathers(tmp_dir=tmp_dir,project_name=project_name_construct)
+
 pbs_base = _os.path.join('/scratch/bogdanm/pbs',project_name) #break down by project folders as gets slow on hpc with multiple files
 project_name_construct = _project_name_construct(project_name,PPPtype,pos_s,wetz_s,tropNom_input,ElMin,ambres)
 #generating tree files that won't be overwritten as crc32 will be the same
@@ -71,7 +87,9 @@ for i in range(len(stations_list_arrays)):
     code = gen_code(stations_list = list(stations_list_arrays[i]), cache_path = cache_path,tropNom_input=tropNom_input, ambres = ambres,ElMin=ElMin,ElDepWeight=ElDepWeight,
                     staDb_path = staDb_path,years_list=years_list,num_cores=num_cores,tmp_dir=tmp_dir,project_name=project_name,IGS_logs_dir=IGS_logs_dir,blq_file=blq_file,
                     VMF1_dir = VMF1_dir,pos_s = pos_s,wetz_s = wetz_s,PPPtype = PPPtype,ionex_type=ionex_type,IONEX_products = IONEX_products,rate = rate,
-                    gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,hatanaka=hatanaka,tree_options = tree_options_code,tqdm=False,
+                    gnss_products_dir = gnss_products_dir,eterna_path=eterna_path,hardisp_path = hardisp_path,rnx_dir=rnx_dir,
+                    hatanaka=hatanaka,cddis=cddis,tree_options = tree_options_code,tqdm=False,
                     command='gd2e();kinematic_project.gather_mGNSS()')
 
     qsub_python_code(code,name='{}{}{}'.format(project_name,str(ElMin) if ElMin != 7 else '',str(i)),email='bogdan.matviichuk@utas.edu.au',cleanup=False,pbs_base = pbs_base)
+    
