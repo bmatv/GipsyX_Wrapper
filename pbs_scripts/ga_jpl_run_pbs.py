d44f575de656143c3ccc9abce2fa01c1d581749e
hammermolotok <32863407+hammermolotok@users.noreply.github.com>
2020-10-15 21:07:26 +1100

    rolling back station list

--- pbs_scripts/ga_jpl_run_pbs.py
+++ pbs_scripts/ga_jpl_run_pbs.py
@@ -22,60 +22,60 @@ gnss_products_dir = '/scratch/bogdanm/Products/JPL_GNSS_Products/source/Final'
 #585 stations in total. Below are 423 GPS sites from 2016-01-01 - now. After preleminary cleaning - 363 sites
 stations_list= [    #'00NA' and '01NA' removed due to SEPPOLANT_X_MF, '02NA'; '3CA2','3DA2','3DA3','3PAK', '4AUG' no files
                     #'4BYO', '4CB2', '4CHR', '4CRN', '4CRY', '4RMA', '8BAL';'7DLN' very leittle files
-        # 'ALBU', 'ALBY', 'ALIC', 'ANDA',#'ADEL', '8BUN' very little files, , 'ADE1', 'ADE2'
-        # 'ANGS', 'ANNA', 'ANTW', 'APSL', 'ARDL', 'ARMC', 'ARMD', 'ARRT',
-        # 'ARUB', 'ASHF', 'BALA', 'BALL', 'BALM', 'BALN',#'BAIR', 'BALI' removed as no files available, 
-        # 'BANK', 'BARR', 'BATH', 'BBOO', 'BCUS', 'BDST', 'BDVL',#'BDLE' removed
-        # 'BEE2', 'BEEC', 'BEGA', 'BEUA', 'BIGG', 'BING', 'BJCT',#'BINN' no files
-        # 'BKNL', 'BLCK', 'BLRN', 'BMAN', 'BNDC', 'BNDY', 'BNLA', 'BOLC',
-        # 'BOMB', 'BOOR', 'BORA', 'BORT', 'BRBA', 'BRDW', 'BRLA', #'BOOL' processing problems with XYZ
-        # 'BRO1', 'BROC', 'BRWN', 'BUCH', 'BULA', 'BUR2', 'BURA',#'BUR1',
-        # 'BURK', 'CANR', 'CARG', 'CBAR', 'CBLA', 'CBLE', 'CBLT',#'BUSS',
-        # 'CBRA', 'CEDU', 'CKWL', 'CLAC', 'CLAH', 'CLBI', 'CLBN', 'CLEV',
-        # 'CLYT', 'CNBN', 'CNDA', 'CNDO', 'COFF', 'COBG', 'COEN', 'COLE', #'COLL',
-        # 'COMA', 'COOB', 'COOL', 'COPS', 'CRAN', 'CRDX', 'CRSY', 'CSNO',
-        # 'CTMD', 'CUT0', 'CWN2', 'CWRA', 'DARW', 'DBBO', 'DKSN',#'CUND',
-        # 'DLQN', 'DODA', 'DORA', 'DORR', 'DRGO', 'DUNE',#'DWEL','DPRT',
-        # 'DWHY', 'EBNK', 'ECHU', 'ECOR', 'EDEN', 'EDSV', 'EPSM',# 'DWNI' removed
-        # 'ERMG', 'ESPA', 'EXMT', 'FLND', 'FORB', 'FORS', 'FROY', 'FTDN',
-        # 'GABO', 'GASC', 'GATT', 'GFEL', 'GFTH', 'GGTN',#'GELA', 'GFTN', 
-        # 'GILG', 'GLB2', 'GLBN', 'GLDN', 'GLEN', 'GLIN', 'GNGN', 'GNOA',
-        # 'GONG', 'GOOL', 'GORO', 'GUNN', 'GURL', #'GROT', 'HAMI','GWAB',
-        # 'HATT', 'HAY1', 'HERN', 'HILL', 'HLBK', #'HMLT', 'HIL1', 'HNIS' removed
-        # 'HNSB', 'HOB2', 'HOTH', 'HRSM', 'HUGH', 'HYDN', 'IHOE', 'INVL',
-        # 'IPSR', 'IRYM', 'JAB2', 'JERI', 'JERV', 'JLCK',#'KAL5', 'JAB1',
-        # 'KALG', 'KARR', 'KAT1', 'KAT2', 'KELN', 'KEPK', 'KGIS',#'KDAL',
-        # 'KILK', 'KILM', 'KIRR', 'KMAN', 'KRNG', 'KTMB', 'KTON', 'KULW',
-        # 'KUNU', 'LALB', 'LAMB', 'LARR', 'LDHI', 'LGOW', 'LIAW', 'LILY',
-        # 'LIPO', 'LIRI', 'LKHT', 'LKYA', 'LONA', 'LORD', 'LOTH', 'LURA',
-        # 'MACK', 'MAFF', 'MAIN', 'MANY', 'MARY', 'MCHL', 'MEDO',#'MDAH',
-        # 'MENA', 'MENO', 'MGRV', 'MIMI', 'MITT', 'MLAK', 'MNDE',#'MIDL',
-        # 'MNGO', 'MNSF', 'MOBS', 'MOOR', 'MOUL', 'MRBA', 'MREE', 'MRNO',
-        # 'MRNT', 'MRO1', 'MRWA', #'MRT1', 'MRT2', 'MRT3', 'MRT4', 'MRT5'; MRT* sites have limited data of ~2yr 
-        # 'MSVL', 'MTBU', 'MTCV', 'MTDN', 'MTEM', 'MTHR', 'MTIS', 'MTMA',
-        # 'MUDG', 'MULG', 'MURR', 'MWAL', 'MYRT', 'NBRI', 'NBRK', 'NCLF',
-        # 'NDRA', 'NEBO', 'NELN', 'NEWE', 'NEWH', 'NGAN', 'NHIL', 'NMBN',
-        # 'NMTN', 'NNOR', 'NORS', 'NRMN', 'NSTA', 'NTJN', 'NWCS',#'NOOS',
-        # 'NWRA', 'NYMA', 'OBRN', 'OMEO', 'ORNG', 'OVAL', 'PACH',#'ORBO',
-        # 'PARK', 'PBOT', 'PERI', 'PERT', 'PIAN', 'PKVL', 'PMAC', 'POCA',
-        # 'POON', 'PRCE', 'PRKS', 'PRTF', 'PTHL', 'PTKL', 'PTSV',#'PTLD',
-        # 'PUTY', 'QCLF', 'QUAM', 'RAND', 'RANK', 'RAVN', 'RBVL', 'RGLN',
-        # 'RHPT', 'RKLD', 'RNBO', 'RNSP', 'ROBI', 'RSBY',#'RNIS','ROTT',
-        # 'RUTH', 'RUUS', 'RYLS', 'SA45', 'SCON', 'SEAL', 'SEMR', 'SKIP',
-        # 'SNGO', 'SPBY', 'SPWD', 'SRVC', 'STHG', 'STNY', 'STR1', 'STR2',
-        # 'STR3', 'STRH', 'SWNH', 'SYDN', 'SYM1', 'TAMW', 'TARE', 'TATU',
-        # 'TBOB', 'TELO', 'THEV', 'THOM', 'TID1', 'TIDB', 'TITG', 'TLPA',
-        # 'TMBA', 'TMBO', 'TMRA', 'TMUT', 'TNTR', 'TOMP', 'TOOG',#'TOOW', 
-        # 'TOTT', 'TOW2', 'TULL', 'TURO', 'UCLA', 'ULLA', 'UNDE',#'TORK',
-        # 'VLWD', 'WAGN', 'WAKL', 'WALW', 'WARA', 'WARI', 'WARW', 'WBEE',
-        # 'WDBG', 'WEDD', 'WEEM', 'WEND', 'WFAL', 'WGGA', #'WEIP','WHIY'
-        # 'WILU', 'WLAL', 'WLCA', 'WLGT', 'WLWN', 'WMGA', 'WORI',#'WOOL',
+        'ALBU', 'ALBY', 'ALIC', 'ANDA',#'ADEL', '8BUN' very little files, , 'ADE1', 'ADE2'
+        'ANGS', 'ANNA', 'ANTW', 'APSL', 'ARDL', 'ARMC', 'ARMD', 'ARRT',
+        'ARUB', 'ASHF', 'BALA', 'BALL', 'BALM', 'BALN',#'BAIR', 'BALI' removed as no files available, 
+        'BANK', 'BARR', 'BATH', 'BBOO', 'BCUS', 'BDST', 'BDVL',#'BDLE' removed
+        'BEE2', 'BEEC', 'BEGA', 'BEUA', 'BIGG', 'BING', 'BJCT',#'BINN' no files
+        'BKNL', 'BLCK', 'BLRN', 'BMAN', 'BNDC', 'BNDY', 'BNLA', 'BOLC',
+        'BOMB', 'BOOR', 'BORA', 'BORT', 'BRBA', 'BRDW', 'BRLA', #'BOOL' processing problems with XYZ
+        'BRO1', 'BROC', 'BRWN', 'BUCH', 'BULA', 'BUR2', 'BURA',#'BUR1',
+        'BURK', 'CANR', 'CARG', 'CBAR', 'CBLA', 'CBLE', 'CBLT',#'BUSS',
+        'CBRA', 'CEDU', 'CKWL', 'CLAC', 'CLAH', 'CLBI', 'CLBN', 'CLEV',
+        'CLYT', 'CNBN', 'CNDA', 'CNDO', 'COFF', 'COBG', 'COEN', 'COLE', #'COLL',
+        'COMA', 'COOB', 'COOL', 'COPS', 'CRAN', 'CRDX', 'CRSY', 'CSNO',
+        'CTMD', 'CUT0', 'CWN2', 'CWRA', 'DARW', 'DBBO', 'DKSN',#'CUND',
+        'DLQN', 'DODA', 'DORA', 'DORR', 'DRGO', 'DUNE',#'DWEL','DPRT',
+        'DWHY', 'EBNK', 'ECHU', 'ECOR', 'EDEN', 'EDSV', 'EPSM',# 'DWNI' removed
+        'ERMG', 'ESPA', 'EXMT', 'FLND', 'FORB', 'FORS', 'FROY', 'FTDN',
+        'GABO', 'GASC', 'GATT', 'GFEL', 'GFTH', 'GGTN',#'GELA', 'GFTN', 
+        'GILG', 'GLB2', 'GLBN', 'GLDN', 'GLEN', 'GLIN', 'GNGN', 'GNOA',
+        'GONG', 'GOOL', 'GORO', 'GUNN', 'GURL', #'GROT', 'HAMI','GWAB',
+        'HATT', 'HAY1', 'HERN', 'HILL', 'HLBK', #'HMLT', 'HIL1', 'HNIS' removed
+        'HNSB', 'HOB2', 'HOTH', 'HRSM', 'HUGH', 'HYDN', 'IHOE', 'INVL',
+        'IPSR', 'IRYM', 'JAB2', 'JERI', 'JERV', 'JLCK',#'KAL5', 'JAB1',
+        'KALG', 'KARR', 'KAT1', 'KAT2', 'KELN', 'KEPK', 'KGIS',#'KDAL',
+        'KILK', 'KILM', 'KIRR', 'KMAN', 'KRNG', 'KTMB', 'KTON', 'KULW',
+        'KUNU', 'LALB', 'LAMB', 'LARR', 'LDHI', 'LGOW', 'LIAW', 'LILY',
+        'LIPO', 'LIRI', 'LKHT', 'LKYA', 'LONA', 'LORD', 'LOTH', 'LURA',
+        'MACK', 'MAFF', 'MAIN', 'MANY', 'MARY', 'MCHL', 'MEDO',#'MDAH',
+        'MENA', 'MENO', 'MGRV', 'MIMI', 'MITT', 'MLAK', 'MNDE',#'MIDL',
+        'MNGO', 'MNSF', 'MOBS', 'MOOR', 'MOUL', 'MRBA', 'MREE', 'MRNO',
+        'MRNT', 'MRO1', 'MRWA', #'MRT1', 'MRT2', 'MRT3', 'MRT4', 'MRT5'; MRT* sites have limited data of ~2yr 
+        'MSVL', 'MTBU', 'MTCV', 'MTDN', 'MTEM', 'MTHR', 'MTIS', 'MTMA',
+        'MUDG', 'MULG', 'MURR', 'MWAL', 'MYRT', 'NBRI', 'NBRK', 'NCLF',
+        'NDRA', 'NEBO', 'NELN', 'NEWE', 'NEWH', 'NGAN', 'NHIL', 'NMBN',
+        'NMTN', 'NNOR', 'NORS', 'NRMN', 'NSTA', 'NTJN', 'NWCS',#'NOOS',
+        'NWRA', 'NYMA', 'OBRN', 'OMEO', 'ORNG', 'OVAL', 'PACH',#'ORBO',
+        'PARK', 'PBOT', 'PERI', 'PERT', 'PIAN', 'PKVL', 'PMAC', 'POCA',
+        'POON', 'PRCE', 'PRKS', 'PRTF', 'PTHL', 'PTKL', 'PTSV',#'PTLD',
+        'PUTY', 'QCLF', 'QUAM', 'RAND', 'RANK', 'RAVN', 'RBVL', 'RGLN',
+        'RHPT', 'RKLD', 'RNBO', 'RNSP', 'ROBI', 'RSBY',#'RNIS','ROTT',
+        'RUTH', 'RUUS', 'RYLS', 'SA45', 'SCON', 'SEAL', 'SEMR', 'SKIP',
+        'SNGO', 'SPBY', 'SPWD', 'SRVC', 'STHG', 'STNY', 'STR1', 'STR2',
+        'STR3', 'STRH', 'SWNH', 'SYDN', 'SYM1', 'TAMW', 'TARE', 'TATU',
+        'TBOB', 'TELO', 'THEV', 'THOM', 'TID1', 'TIDB', 'TITG', 'TLPA',
+        'TMBA', 'TMBO', 'TMRA', 'TMUT', 'TNTR', 'TOMP', 'TOOG',#'TOOW', 
+        'TOTT', 'TOW2', 'TULL', 'TURO', 'UCLA', 'ULLA', 'UNDE',#'TORK',
+        'VLWD', 'WAGN', 'WAKL', 'WALW', 'WARA', 'WARI', 'WARW', 'WBEE',
+        'WDBG', 'WEDD', 'WEEM', 'WEND', 'WFAL', 'WGGA', #'WEIP','WHIY'
+        'WILU', 'WLAL', 'WLCA', 'WLGT', 'WLWN', 'WMGA', 'WORI',#'WOOL',
         'WOTG', 'WRRN', 'WTCF', 'WWLG', 'WYCH', 'WYNG', 'YALL', 'YANK',
         'YAR2', 'YAR3', 'YARO', 'YARR', 'YARS', 'YASS', 'YEEL',#'YAR1', 
         'YELO', 'YIEL', 'YMBA', 'YNKI', 'YRRM', 'YULA', 'YUNG']
 
 years_list=[2013,2014,2015,2016,2017,2018,2019,2020];num_cores = 28
-num_nodes = 30 #default is 10 . nz gd2e shows full load of 20 nodes
+num_nodes = 20 #default is 10 . nz gd2e shows full load of 20 nodes
 if num_nodes > len(stations_list): num_nodes = len(stations_list) #in case staions num is less than num_nodes => num_nodes = stations num
 #-------------------------------------------------------------------------------------------------------------------
 #We need to generate unique staDb with all the stations
